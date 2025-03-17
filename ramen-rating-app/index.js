b9f76fd17488399e3d94ed02c7b526637d38a622
DReagan90 <reagan.baimet@student.moringaschool.com>
2025-03-18 00:31:57 +0300

    Add files via upload

---
+++ ramen-rating-app/index.js
@@ -0,0 +1,14 @@
+const ramenMenu = document.getElementById("ramen-menu")
+const detailImage =document.getElementById("detail-image")
+const ratingDisplay = document.getElementById('Rating-display')
+const commentDisplay = document.getElementById('Comment-display')
+const newRamenForm = document.getElementById('new-ramen-form')
+
+
+    const ramens = [
+        { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+        { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+        { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+     ];
+
+     
